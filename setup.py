b3d19b6098e359765515ece43b6026e5d7f8488b
Robert Dyro <rdyro@stanford.edu>
2023-08-28 18:06:06 -0700

    added a setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xplane_vision",
+    version="0.1",
+    packages=find_packages(),
+    author="Robert Dyro",
+    author_email="rdyro@stanford.edu"
+)
